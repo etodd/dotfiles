445698c86270e4de887a4e350eae5d8131aa290f
Evan Todd <et1337@gmail.com>
2014-08-21 13:31:12 -0400

    Add GTK themes, remove screen visual bell, configure i3 to run screen inside xterm

--- ui.sh
+++ ui.sh
@@ -3,7 +3,11 @@
 set -u
 set -e
 set -o pipefail
- 
+
+# Spotify repo
+sudo bash -c 'echo deb http://repository.spotify.com stable non-free >> /etc/apt/sources.list'
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59
+
 sudo apt-get update
 sudo apt-get upgrade -y
  
@@ -17,9 +21,6 @@ sudo dpkg -i google-chrome*.deb
 rm google-chrome*.deb
  
 # Spotify
-sudo bash -c 'echo deb http://repository.spotify.com stable non-free >> /etc/apt/sources.list'
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59
-sudo apt-get update
 sudo apt-get install -y spotify-client
  
 # Dropbox
