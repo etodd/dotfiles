bded4c21b2b90a7f28d07555d1714aff49845b39
Evan Todd <et1337@gmail.com>
2014-11-18 16:00:23 -0500

    Install gsettings

--- ui.sh
+++ ui.sh
@@ -8,7 +8,7 @@ sudo apt-get update
 sudo apt-get upgrade -y
  
 #i3
-sudo apt-get install xserver-xorg lightdm nautilus gnome-font-viewer eog i3 scrot network-manager firefox
+sudo apt-get install xserver-xorg lightdm nautilus gnome-font-viewer eog i3 scrot network-manager firefox libglib2.0-bin
  
 # Disable nautilus desktop
 gsettings set org.gnome.desktop.background show-desktop-icons false
