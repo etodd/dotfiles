478acb427653f3974b4809ea853f358050d7e550
Evan Todd <et1337@gmail.com>
2014-11-18 15:55:18 -0500

    Chrome -> Firefox

--- ui.sh
+++ ui.sh
@@ -8,15 +8,7 @@ sudo apt-get update
 sudo apt-get upgrade -y
  
 #i3
-sudo apt-get install xserver-xorg lightdm nautilus gnome-font-viewer eog i3 scrot network-manager
- 
-# Chrome
-sudo apt-get install -y libxss1
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome*.deb || true # This will fail if dependencies are not already met
-sudo apt-get install -f # Install Chrome dependencies
-sudo dpkg -i google-chrome*.deb # Now install the Chrome deb again
-rm google-chrome*.deb
+sudo apt-get install xserver-xorg lightdm nautilus gnome-font-viewer eog i3 scrot network-manager firefox
  
 # Disable nautilus desktop
 gsettings set org.gnome.desktop.background show-desktop-icons false
