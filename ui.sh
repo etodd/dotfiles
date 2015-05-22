77a00290af0249568635f9c5577940e04c2be08b
Evan Todd <et1337@gmail.com>
2015-05-22 13:24:34 -0400

    Minor tweaks

--- ui.sh
+++ ui.sh
@@ -8,7 +8,7 @@ sudo apt-get update
 sudo apt-get upgrade -y
  
 #i3
-sudo apt-get install xserver-xorg x11-xserver-utils xterm lightdm nautilus gnome-font-viewer eog i3 scrot network-manager firefox libglib2.0-bin vim-gtk gnome-control-center ubuntu-session gnome-session alsa-utils
+sudo apt-get install -y xserver-xorg x11-xserver-utils xterm lightdm nautilus gnome-font-viewer eog i3 scrot network-manager firefox libglib2.0-bin vim-gtk gnome-control-center ubuntu-session gnome-session alsa-utils
  
 # Disable nautilus desktop
 gsettings set org.gnome.desktop.background show-desktop-icons false
