9807c0d8523ebe2b5a8b174cd617e9098dfc07cb
Evan Todd <evan@etodd.io>
2023-12-29 15:13:41 -0800

    remove linux-specific stuff. add alacritty and aerospace

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,24 +1,23 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "${BASH_SOURCE}")";
+set -e
 
-git pull origin master;
-git submodule update --init --recursive;
+cd "$(dirname "${BASH_SOURCE}")"
+
+git submodule update --init --recursive
 
 function doIt() {
 	rsync --exclude ".git/" --exclude "/*.sh" \
-		--exclude "README.md" --exclude ".gitmodules" -avh --no-perms . ~;
-	source ~/.bash_profile;
-	xrdb -merge ~/.Xresources
+		--exclude "README.md" --exclude ".gitmodules" -avh --no-perms . ~
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
+	doIt
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+	echo ""
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
-fi;
-unset doIt;
+		doIt
+	fi
+fi
+unset doIt
