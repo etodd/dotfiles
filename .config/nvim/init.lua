d01d6cff4c798dee13cb2d851dd5a1577d1b2311
Evan Todd <evan@etodd.io>
2026-01-25 15:50:13 -0800

    try new vimpack instead of lazyvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,19 +1,6 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup({
-  { "ctrlpvim/ctrlp.vim" },
-  { "mg979/vim-visual-multi", branch = "master" },
+vim.pack.add({
+  { src = "https://github.com/ctrlpvim/ctrlp.vim" },
+  { src = "https://github.com/mg979/vim-visual-multi", branch = "master" },
 })
 
 local opt = vim.opt
